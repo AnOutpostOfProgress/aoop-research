2f30671858153257a05890f7d8c417e708693777
ffresh <38914629+ffresh@users.noreply.github.com>
2021-01-18 12:27:41 +0100

    Workout-Timer_v0.2
    Einstellmöglichkeit von Aktivzeit, Pausenzeit und Anzahl von Wiederholungen der Sätze. Start/Stopp über Play-Button.

--- workout-timer/assets/js/main.js
+++ workout-timer/assets/js/main.js
@@ -1,41 +1,132 @@
+const btnPlay = document.getElementById('btn-play');
+const btnArith = document.querySelectorAll('.btn-arith');
+
+let timerIsRunning = false;
+let timerInstance, pauseInstance;
+
 const beep01 = document.getElementById("beep01");
 const beep02 = document.getElementById("beep02");
-const buttons = document.querySelectorAll('.button');
 
-buttons.forEach(item =>{
+
+btnPlay.addEventListener('click', event =>{
+
+  document.getElementById('triangle').classList.toggle('displayNone');
+  document.getElementById('square').classList.toggle('displayNone');
+
+  btnArith.forEach(item =>{
+    item.classList.toggle('hidden');
+  });
+
+  timerIsRunning ? stopTimer() : startTimer();
+
+}); 
+
+
+btnArith.forEach(item =>{
   item.addEventListener('click', event =>{
-    
-    item.classList.toggle('active');
-    beep01.play();
-    
-    let display = document.querySelector("#" + CSS.escape(item.id) + " span");
-    let timer = display.textContent; 
-    let btnValue = timer;
-    let seconds;
 
-    --timer;
-    
-    setInterval(function () {
-      seconds = parseInt(timer % 60, 10);
-      seconds = seconds < 10 ? "0" + seconds : seconds;
-      display.textContent = seconds;
+    const counter = item.parentNode.children[2]; //access to corresponding counter-element
+    const arithVal = parseInt(item.getAttribute('data-arith'));
+   
+    let dataCounter = parseInt(counter.getAttribute('data-counter')) + arithVal;
 
-      if (--timer < 0) {
-          clearAllIntervals();
-          beep02.play();
-          display.textContent = btnValue;
-          item.classList.toggle("active");
-      }
-    }, 1000);
-    
+    if(dataCounter > 0){
+      counter.setAttribute('data-counter', dataCounter);
+      counter.innerHTML = dataCounter;
+    }
+
   })
-});
+})
 
 
-function clearAllIntervals(){                                           
-  for (let i = 1; i < 999; i++){
-    window.clearInterval(i);
+function startTimer(){
+
+  timerIsRunning = true;
+  
+  const timer = document.getElementById('timer');
+  const pause = document.getElementById('pause');
+
+  let timerVal = document.getElementById('timerVal');
+  let pauseVal = document.getElementById('pauseVal');
+  let repVal = document.getElementById('repVal');
+
+  if(parseInt(repVal.innerHTML)!=0){
+    
+    timer.classList.toggle('active');
+    document.querySelector('#timer object').classList.replace('greenIcon', 'whiteIcon');
+    beep01.play();
+
+    timerInstance = setInterval( () =>{
+      
+      timerVal.innerHTML = parseInt(timerVal.innerHTML) - 1;
+
+      if (parseInt(timerVal.innerHTML)==0){
+
+        window.clearInterval(timerInstance);
+        repVal.innerHTML = parseInt(repVal.innerHTML) - 1;
+        timerVal.innerHTML = timerVal.getAttribute('data-counter');
+        timer.classList.toggle('active');
+        document.querySelector('#timer object').classList.replace('whiteIcon', 'greenIcon');
+        beep02.play();
+
+        if(parseInt(repVal.innerHTML)!=0){
+          
+          pause.classList.toggle('active');
+          document.querySelector('#pause object').classList.replace('greenIcon', 'whiteIcon');
+          
+          pauseInstance = setInterval( () =>{
+            
+            pauseVal.innerHTML = parseInt(pauseVal.innerHTML) - 1;
+            
+            if (parseInt(pauseVal.innerHTML)==0){
+              window.clearInterval(pauseInstance);
+              pauseVal.innerHTML = pauseVal.getAttribute('data-counter');
+              pause.classList.toggle('active');
+              document.querySelector('#pause object').classList.replace('whiteIcon', 'greenIcon');
+
+              if(parseInt(repVal.innerHTML)!=0){
+                startTimer();
+              }
+            }
+          },1000)
+
+        }else{
+          stopTimer();
+          //setTimeout(beep02.play(),1000); //optional final finish sound
+        }
+      }
+
+
+    },1000);
   }
+}
+
+
+function stopTimer(){
+
+  window.clearInterval(timerInstance);
+  window.clearInterval(pauseInstance);
+
+  timerIsRunning = false;
+
+  document.getElementById('timerVal').innerHTML = document.getElementById('timerVal').getAttribute('data-counter');
+  document.getElementById('pauseVal').innerHTML = document.getElementById('pauseVal').getAttribute('data-counter');
+  document.getElementById('repVal').innerHTML = document.getElementById('repVal').getAttribute('data-counter');
+  
+  document.getElementById('timer').classList.remove('active');
+  document.getElementById('pause').classList.remove('active');
+  document.getElementById('triangle').classList.remove('displayNone');
+  document.getElementById('square').classList.add('displayNone');
+
+  document.querySelector('#timer object').classList.add('greenIcon');
+  document.querySelector('#timer object').classList.remove('whiteIcon');
+  document.querySelector('#pause object').classList.add('greenIcon');
+  document.querySelector('#pause object').classList.remove('whiteIcon');
+
+  btnArith.forEach(item =>{
+    item.classList.remove('hidden');
+  });
+  
 }
 
 
